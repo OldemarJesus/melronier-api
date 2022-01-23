528913923a0fb158d35cbf44b4f60bd8df5753f5
Oldemar <oldemego@gmail.com>
2022-01-23 13:18:26 +0000

    changed script

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-
+const port = 3000;
 
 app.get('/', function(req:Request, res:Response){
     res.send("Hello World");
+})
+
+app.listen(port, () => {
+        console.log(`server start at port ${port}`)
 })
