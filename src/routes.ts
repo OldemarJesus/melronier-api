7cc2896ccd1c633d24dafbe2cb1aef9f89277674
Oldemar <oldemego@gmail.com>
2022-01-30 13:41:03 +0000

    add cors to post request

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
-import {NextFunction, Request, Response, Router} from 'express';
+import {Router} from 'express';
+import cors from 'cors';
 import AgendamentoController from './controllers/AgendamentoController';
 const validateDto = require('../src/middleware/validate-dto');
 const devDto = require('../src/dto/agendamento');
@@ -9,6 +10,6 @@ routes.get('/', AgendamentoController.home);
 
 routes.get('/agendamentos', AgendamentoController.index);
 
-routes.post('/agendamentos', validateDto(devDto), AgendamentoController.create);
+routes.post('/agendamentos', cors(), validateDto(devDto), AgendamentoController.create);
 
 export default routes;
